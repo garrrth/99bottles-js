55f476f3b32904dda5f77a4e79a37db7323d9326
Gareth Thomas <gareth.thomas@raytheon.co.uk>
2025-07-30 15:33:19 +0100

    another fix!

--- index.spec.js
+++ index.spec.js
@@ -2,5 +2,5 @@ import { expect, test } from 'vitest'
 
 test('first verse', () => {
   const result = new Bottles().verse(99)
-  expect(result).toBe('99 bottles of beer on the wall, 99 bottles of beer\nTake one down and pass it around, 99 bottles of beer on the wall\n\n')
+  expect(result).toBe('99 bottles of beer on the wall, 99 bottles of beer\nTake one down and pass it around, 98 bottles of beer on the wall\n\n')
 })
