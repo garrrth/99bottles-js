c5053fec14ef7c1cf3aa453c5976f82972689966
Gareth Thomas <gareth.thomas@raytheon.co.uk>
2025-07-30 15:46:40 +0100

    minor update

--- index.spec.js
+++ index.spec.js
@@ -1,6 +1,7 @@
 import { expect, test } from 'vitest'
+import { Bottles } from './index'
 
 test('first verse', () => {
   const result = new Bottles().verse(99)
-  expect(result).toBe('99 bottles of beer on the wall, 99 bottles of beer\nTake one down and pass it around, 98 bottles of beer on the wall\n\n')
+  expect(result).toEqual('99 bottles of beer on the wall, 99 bottles of beer\nTake one down and pass it around, 98 bottles of beer on the wall\n\n')
 })
